80b93b6fd057a223e6f3cd10606ed5fa61ec4afe
Vitor Araujo <vitoraraujo.dev@gmail.com>
2020-03-25 20:47:20 -0300

    Login e Páginação de Incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
@@ -15,5 +16,6 @@ routes.delete('/incidents/:id', IncidentController.delete);
 
 routes.get('/profile', ProfileController.index);
 
+routes.post('/sessions', SessionController.create);
 
 module.exports = routes;
