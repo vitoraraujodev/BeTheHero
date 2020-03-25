21b24ef0ffa1af91dc36080abe5977df8856ec08
Vitor Araujo <vitoraraujo.dev@gmail.com>
2020-03-25 20:53:20 -0300

    Relação entre Ongs e Incidents / Cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
-
+app.use(cors()); //opção 'origin' para definir url
 app.use(express.json());
 app.use(routes);
 
