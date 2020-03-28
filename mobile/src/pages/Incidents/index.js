7f56a9bbbd177f70af4033587ef4a0245b3d62b9
Vitor Araujo <vitoraraujo.dev@gmail.com>
2020-03-28 16:54:35 -0300

    Estilização da página Incidents

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,10 +1,55 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'
+import { View, Image, Text, TouchableOpacity, FlatList } from 'react-native';
 
-// import { Container } from './styles';
+import styles from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate('Detail');
+  }
+
   return (
-    <View />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText} >
+          Total de <Text style={styles.headerTextBold}>0 casos.</Text>
+        </Text>
+      </View>
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text styles={styles.description}>Escolha um caso e salve o dia.</Text>
+
+      <FlatList
+        data={[1, 2, 3]}
+        style={styles.incidentList}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+        <View style={styles.incident}>
+        <Text style={styles.incidentProperty}>ONG:</Text>
+        <Text style={styles.incidentValue}>ONG:</Text>
+
+        <Text style={styles.incidentProperty}>CASO:</Text>
+        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+        <Text style={styles.incidentProperty}>VALOR:</Text>
+        <Text style={styles.incidentValue}>R$ 120,00</Text>
+      
+        <TouchableOpacity
+          style={styles.detailsButton}
+          onPress={navigateToDetail}
+        >
+          <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+          <Feather name="arrow-right" size={16} color="#E02041" />
+        </TouchableOpacity>
+      </View>
+      )} />
+    </View>
   );
 }
